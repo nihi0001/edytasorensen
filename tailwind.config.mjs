e1e47640c73d4b6ac33902535587943ef00bee2b
Nina <nihi0001@kea.dk>
2024-12-04 12:30:07 +0100

    tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+	  extend: {
+		colors: {
+		  'custom-bg': '#FFFCFA', // baggrundsfarve
+		},
+		fontFamily: {
+		  'old-standard': ['"Old Standard TT"', 'serif'],  // Tilføjer Old Standard TT fonten
+		  'another-font': ['""', 'sans-serif'], // 
+		},
+	  },
 	},
 	plugins: [],
-}
+  }
+  
